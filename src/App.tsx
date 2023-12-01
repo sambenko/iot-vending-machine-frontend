640bbadadab6fc63fdbce6ffe3f2ca7596b5fc30
sambenko <sam.benko@gmail.com>
2023-12-01 19:00:55 +0100

    provide theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
-import DeviceStats from './components/DeviceStats';
+import DeviceStats from './components/DeviceStats/DeviceStats';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme';
+import { Box, CssBaseline, Grid } from '@mui/material';
 
-function App() {
+const App: React.FC = () => {
+  const espboxliteImage = process.env.PUBLIC_URL + '/esp32_s3_box_lite.png';
+  const espboxImage = process.env.PUBLIC_URL + '/esp32_s3_box.png';
   return (
-    <div className="App">
-      <h1>IoT Vending Machines</h1>
-      <DeviceStats deviceId="espboxlite" />
-      <DeviceStats deviceId="espbox" />
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh' }}>
+        <Grid container justifyContent="center" spacing={4}>
+          <Grid item>
+            <DeviceStats deviceId="espboxlite" deviceImage={espboxliteImage} />
+          </Grid>
+          <Grid item>
+            <DeviceStats deviceId="espbox" deviceImage={espboxImage} />
+          </Grid>
+        </Grid>
+      </Box>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
