ec7c1089f3046fd170ceed16465c3031ac7641ec
sambenko <sam.benko@gmail.com>
2023-12-01 15:32:46 +0100

    IoT vending machined

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import DeviceStats from './components/DeviceStats';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>IoT Vending Machines</h1>
+      <DeviceStats deviceId="espboxlite" />
+      <DeviceStats deviceId="espbox" />
     </div>
   );
 }
